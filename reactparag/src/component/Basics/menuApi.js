514ab90dde89a1975d066511b23af0df14c40b30
Parag Nimkar <nimkarparag5@gmail.com>
2023-12-12 17:41:34 +0530

    description change in content of menu

--- reactparag/src/component/Basics/menuApi.js
+++ reactparag/src/component/Basics/menuApi.js
@@ -6,7 +6,7 @@ const Menu = [
     category: "breakfast",
     price: "25₹",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, voluptates.",
+      "Maggi is a popular instant noodle brand known for its quick preparation, distinct spices, and wide varieties. A convenient and tasty snack enjoyed globally.",
   },
   {
     id: 2,
@@ -15,34 +15,34 @@ const Menu = [
     category: "evening",
     price: "20₹",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      "Aloo Pakora is a popular Indian street food snack made with spiced potatoes and gram flour batter. These crispy fritters are a popular monsoon snack and can be made in minutes. ",
   },
   {
     id: 3,
     image: "images/corn.jpg",
-    name: "corn",
+    name: "Corn",
     category: "breakfast",
     price: "10₹",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      "Corn is a starchy vegetable and cereal grain that has been eaten all over the world for centuries. It's rich in fiber, vitamins and minerals. However, the health benefits of corn are controversial — while it contains beneficial nutrients, it can also spike blood sugar levels.",
   },
   {
     id: 4,
     image: "../images/chola.jpg",
-    name: "chola",
+    name: "Chola",
     category: "lunch",
     price: "50₹",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      "Chole is a food dish originating from northern India. However, it is known in most parts of India. It is a combination of chana masala and bhatura/Puri, a fried bread made from maida. ",
   },
   {
     id: 5,
     image: "../images/pizza.jpg",
-    name: "pizza",
+    name: "Pizza",
     category: "evening",
     price: "80₹",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      "Pizza is a savory dish of Italian origin consisting of a usually round, flattened base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients ",
   },
   {
     id: 6,
@@ -51,7 +51,7 @@ const Menu = [
     category: "dinner",
     price: "180₹",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      "Non-Veg Thali is a food dish originating from northern India. However, it is known in most parts of India. It is a combination of chana masala and bhatura/Puri, a fried bread made from maida.",
   },
   {
     id: 7,
@@ -60,25 +60,25 @@ const Menu = [
     category: "dinner",
     price: "60₹",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      "Sweets is a savory dish of Italian origin consisting of a usually round, flattened base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients ",
   },
   {
     id: 8,
     image: "../images/rajmarice.jpg",
-    name: "Rajma Rice",
+    name: "Rajma Chawal",
     category: "lunch",
     price: "60₹",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      "Rajma Rice is a food dish originating from northern India. However, it is known in most parts of India. It is a combination of chana masala and bhatura/Puri, a fried bread made from maida.",
   },
   {
     id: 9,
     image: "../images/samosa.jpg",
-    name: "samaso",
+    name: "Samosa",
     category: "evening",
     price: "10₹",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      "Samosa is a fried or baked pastry with a savory filling, such as spiced potatoes, onions, peas, cheese, beef and other meats, or lentils. It may take different forms, including triangular, cone, or half-moon shapes, depending on the region.",
   },
 ];
 
